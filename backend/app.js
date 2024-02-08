2a5baf8091f0838bd1b0c80ab405053c6af67d78
joellaprade <145079600+joellaprade@users.noreply.github.com>
2024-02-08 13:00:02 -0600

    d

--- backend/app.js
+++ backend/app.js
@@ -67,8 +67,8 @@ app.get('/create-event', async (req, res) => {
             minute: 0
         },
         tag: '232352'
-    })
-
+    }).save();
+    res.send(200)
 })
 
 app.get('/administrar-tareas', (req, res) => {
